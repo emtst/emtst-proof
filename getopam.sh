7e319a3ef1824e300818f8df06b0cfc255fe66a2
emtst <emtst@42nd.ca>
2019-10-11 16:23:28 +0100

    added missing dependency, m4

--- getopam.sh
+++ getopam.sh
@@ -3,6 +3,6 @@
 echo 'Update'
 apt-get -y update && apt-get -y upgrade || { echo 'apt update failed' ; exit 1; }
 
-echo 'Install opam 2.0'
-add-apt-repository -y ppa:avsm/ppa &&
+echo 'Install opam 2.0 and m4'
+apt-get -y install m4 && add-apt-repository -y ppa:avsm/ppa &&
 apt-get -y install opam || { echo 'installing opam failed' ; exit 1; }
