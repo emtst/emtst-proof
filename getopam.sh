2c00f481f4f2691e5e6d10f8cbd49e0a1a82620f
emtst <emtst@42nd.ca>
2019-10-11 16:31:38 +0100

    udpates

--- getopam.sh
+++ getopam.sh
@@ -4,5 +4,5 @@ echo 'Update'
 apt-get -y update && apt-get -y upgrade || { echo 'apt update failed' ; exit 1; }
 
 echo 'Install opam 2.0 and m4'
-apt-get -y install m4 && add-apt-repository -y ppa:avsm/ppa &&
+apt-get -y install m4 && add-apt-repository -y ppa:avsm/ppa && \
 apt-get -y install opam || { echo 'installing opam failed' ; exit 1; }
--- prereq.sh
+++ prereq.sh
@@ -4,12 +4,11 @@ echo 'installing opam (and your password will be required.)'
 sudo ./getopam.sh || { echo 'failed to install opam.' ; exit 1; }
 
 echo 'configure opam.'
-opam init -y
-    && eval $(opam config env)
-    && opam -y repo add coq-released https://coq.inria.fr/opam/released
+opam init -y && eval $(opam config env) \
+    && opam -y repo add coq-released https://coq.inria.fr/opam/released \
     || { echo 'failed to initialise opam and the Coq repo.' ; exit 1; }
 
 echo 'install coq and ssreflect.'
-opam install -y coq.8.9.0
-    && opam install -y coq-mathcom-ssreflect.1.7.0
+opam install -y coq.8.9.0 \
+    && opam install -y coq-mathcom-ssreflect.1.7.0 \
     || { echo 'failed to initialise opam and the Coq repo.' ; exit 1; }
