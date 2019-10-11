44a5be02f877dc01d4562ca7e664b0b985cf7578
emtst <emtst@42nd.ca>
2019-10-11 15:02:58 +0100

    adding configuration scripts

---
+++ getopam.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 'Update'
+apt-get -y update && apt-get -y upgrade || { echo 'apt update failed' ; exit 1; }
+
+echo 'Install opam 2.0'
+add-apt-repository -y ppa:avsm/ppa &&
+apt-get -y install opam || { echo 'installing opam failed' ; exit 1; }
